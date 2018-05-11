4d660da6ea85fd3253937605bf0911890464934e
Kathy <wknjenny@gmail.com>
2018-05-12 00:07:46 +1000

    Finish most of functions

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,11 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./users');
 
 /* GET dogs listing. */
 router.get('/', function(req, res, next) {
-    res.render('dogProfile', {
-        type: 'dogs'
-    });
+    var connection = users.startMysql();
+    connection.query("SELECT * FROM dogs WHERE email = ?", users.getEmail(), function (err, result) {
+        if (err) {
+            console.log("Select dog error: " + err);
+        } else {
+            var jsonresult = JSON.parse(JSON.stringify(result));
+            res.render('dogProfile', {
+                type: 'dogs',
+                info: jsonresult
+            });
+            connection.end();
+        }
+    })
 });
+
+
+/* Add dog */
+router.post('/add', function (req, res) {
+    var connection = users.startMysql();
+    var dog = {
+        email: users.getEmail(),
+        name: req.body.name,
+        breed: req.body.breed,
+        birthday: req.body.birthday
+    }
+    connection.query('INSERT INTO dogs SET ?', dog, function (err, result) {
+        if (err) {
+            console.log("Insert dog error: " + err);
+        }
+        else {
+            connection.query("SELECT * FROM dogs WHERE email = ?", users.getEmail(), function (err, result) {
+                if (err) {
+                    console.log("Select dog error: " + err);
+                } else {
+                    var jsonresult = JSON.parse(JSON.stringify(result));
+                    res.render('dogProfile', {
+                        type: 'dogs',
+                        info: jsonresult
+                    });
+                    connection.end();
+                }
+            })
+        }
+    })
+})
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,161 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var email = null;
 
 /* user profile */
 router.get('/', function (req, res) {
-    var connection = startMysql();
-    var sql = "SELECT * FROM user where email = 'user1'";
-    connection.query(sql,
-        function (error, results) {
-            if (error) {
-                console.log(error.toString());
-            }
-            else {
-                // substitute with session
-                res.render('userProfile', {
-                    email: 'user1@gmail.com',
-                    type: 'userProfile'
-                     });
-            }
-
-        }
-    );
+    getUser(req, res, false);
 })
 
 
 router.post('/updateProfile', function (req, res) {
-    console.log(req.body.address);
+    var connection = startMysql();
+    var address = req.body.address;
+    var contacts = req.body.contact;
+    console.log(req.body);
+    var sql;
+
+    sql = "UPDATE user SET address = ? WHERE email = ?";
+    console.log(sql);
+    connection.query(sql, [address, email], function (err, result) {
+        if (err) {
+            console.log("Update error", +err.toString());
+        } else {
+            console.log('Update address success');
+        }
+    })
+
+
+    if (contacts != null) {
+        if (contacts[0]) {
+            console.log("Contact:" + contacts.toString());
+
+            sql = "DELETE FROM usercontact WHERE email = '" + email + "'";
+
+            connection.query(sql, function (err, result) {
+                if (err) {
+                    console.log("Delete contact error: " + err.toString());
+                }
+                else {
+                    sql = "INSERT INTO usercontact (email, contact) VALUES ";
+                    for (i = 0; i < contacts.length; i++) {
+                        sql += "( '" + email + "' , '" + contacts[i] + "' " + "), ";
+                    }
+                    sql = sql.replace(/, $/, "");
+                    console.log("Update sql is : " + sql);
+                    connection.query(sql, function (err, result) {
+                        if (err) {
+                            console.log("Insert contacts error: " + err.toString());
+                        }
+                    });
+                    res.send({});
+                    connection.end();
+                }
+            });
+        }
+    } else {
+        res.send({});
+        connection.end();
+    }
+
+
 })
 
 /* login */
 router.post('/login', function(req, res) {
-    storeUser(req)
-    res.render('appoint', {type: 'dogs'});
+    getUser(req, res, true);
 });
 
 /* register */
 router.post('/register', function(req, res) {
-    storeUser(req)
-    res.render('appoint', {type: 'appoints'});
-});
-
-
-
-function storeUser(req){
-    var email = req.body.email;
+    email = req.body.email;
     var psw = req.body.psw;
     var connection = startMysql();
     var user = {
         email : email,
         password : psw
     }
+
     connection.query('INSERT INTO user SET ?', user, function(err, result){
         if(err){
             console.log(err.toString());
         }else{
-            console.log('success');
+            console.log('Register success');
         }
+        var info = {"email": email};
+        res.render('userProfile', {
+            info: info,
+            type: 'userProfile',
+
+        });
     })
     connection.end();
+
+});
+
+
+function getUser(req, res, requirePsw) {
+    if (requirePsw) {
+        email = req.body.email;
+    }
+    var connection = startMysql();
+    var address = null;
+    var info;
+    var contacts = [];
+
+
+    var sql = "SELECT * FROM user WHERE email = '" + email + "'";
+    if (requirePsw) {
+        sql += " and password = '" + req.body.psw + "'"
+    }
+    console.log(sql);
+    connection.query(sql, function (err, result) {
+        if (err) {
+            email = null;
+            console.log("Select user error: " + err.toString());
+
+        } else {
+            console.log('Fetch user success');
+
+            var jsonresult = JSON.parse(JSON.stringify(result));
+            address = jsonresult[0].address;
+
+            // select contacts
+            sql = "SELECT * FROM usercontact WHERE email = ?";
+            connection.query(sql, email, function (err, result) {
+                if (err) {
+                    console.log("Select contact error: " + err.toString());
+
+                } else {
+
+                    var jsonresult = JSON.parse(JSON.stringify(result));
+                    console.log(jsonresult);
+                    for (i = 0; i < jsonresult.length; i++) {
+                        contacts.push(jsonresult[i].contact);
+                    }
+                }
+                info = {
+                    "email": email,
+                    "address": address,
+                    "contacts": contacts
+                }
+
+
+                res.render('userProfile', {
+                    info: info,
+                    type: 'userProfile',
+                });
+                connection.end();
+            })
+        }
+    })
+
+
 }
 
 
 function startMysql(){
-    var mysql      = require('mysql');
     var connection = mysql.createConnection({
         host     : 'localhost',
         user     : 'keningw',
@@ -73,4 +167,9 @@ function startMysql(){
     return connection;
 }
 
+
 module.exports = router;
+module.exports.getEmail = function () {
+    return email;
+};
+module.exports.startMysql = startMysql;
